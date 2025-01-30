2fc13fd9ad69c8326a2a62f48107d6018cd55252
Lilianpat <113841280+Lilianpat@users.noreply.github.com>
2025-01-30 03:41:58 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const hamburgerMenu = document.querySelector('.hamburger-menu');
+  const navLinks = document.querySelector('.nav-links');
+  hamburgerMenu.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+  });
+});
